d361c8c7e82b87a97df9b6bad103b3d681e22771
Ilmir Sharifullin <77682814+ilmir4ik@users.noreply.github.com>
2022-12-05 21:11:20 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 window.onload = function () {
     document.addEventListener('click', documentActions)
 
@@ -6,33 +5,38 @@ window.onload = function () {
         const targetElement = e.target
 
         if (targetElement.classList.contains('add-button')) {
-            console.log("bit")
             const passengers = document.querySelector('.passengers')
             const passengersValue = document.querySelectorAll('.passenger-container')
-            const newPassenger = `<br>
-<div class="passenger-container">
+            const newPassenger = `<div id="${passengersValue.length + 1}" class="passenger-container">
                         <div class="passenger-section">
-                            <div class="passenger-number">Пассажир № ${passengersValue.length+1}</div>
-                            <div class="passenger-add">
-                                <button type="button" class="add-button button-blue">Добавить пассажира</button>
+                            <div class="passenger-number">Пассажир № ${passengersValue.length + 1}</div>
+                            <div class="passenger-block">
+                                <button class="close-button" type="button"><img id="${passengersValue.length + 1}" class="close-button" src="../images/close.svg" alt="Закрыть"></button>
                             </div>
                         </div>
                         <div class="passenger-info">
                             <div class="input-block">
                                 <div class="input-title">Фамилия<span class="red-text">*</span></div>
-                                <input x-moz-errormessage="Это поле обязательное" required type="text" class="input-form">
+                                <input x-moz-errormessage="Это поле обязательное" id="surname" required type="text" class="input-form">
                             </div>
                             <div class="input-block">
                                 <div class="input-title">Имя<span class="red-text">*</span></div>
-                                <input required type="text" class="input-form">
+                                <input required type="text" id="name" class="input-form">
                             </div>
                             <div class="input-block">
                                 <div class="input-title">Отчество<span class="red-text">*</span></div>
-                                <input required type="text" class="input-form">
+                                <input required type="text" id="middlename" class="input-form">
                             </div>
                         </div>
                     </div>`
             passengers.insertAdjacentHTML('beforeend', newPassenger)
+        }
+        if (targetElement.classList.contains('close-button')) {
+            console.log(targetElement.id)
+            const passengers = document.querySelector('.passengers')
+            const deletePas = document.getElementById(targetElement.id)
+            console.log(deletePas)
+            deletePas.remove()
         }
     }
 }
